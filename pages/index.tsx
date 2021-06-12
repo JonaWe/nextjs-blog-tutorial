5c4e8729f3879bbc6eadea11c53b9506fdaccb4a
Jona Wessendorf <jona.wessendorf1@uni-bielefeld.de>
2021-06-12 16:44:17 +0200

    converts project to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,15 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) {
   return (
     <Layout home>
       <Head>
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,8 +3,17 @@ import Head from 'next/head';
 import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import utilStyles from '../../styles/utils.module.css';
+import { GetStaticProps, GetStaticPaths } from 'next';
 
-export default function Post({ postData }) {
+export default function Post({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
   return (
     <Layout>
       <Head>
@@ -21,15 +30,15 @@ export default function Post({ postData }) {
   );
 }
 
-export async function getStaticProps({ params: { id } }) {
-  const postData = await getPostData(id);
+export const getStaticProps: GetStaticProps = async ({ params: { id } }) => {
+  const postData = await getPostData(id as string);
   return { props: { postData } };
-}
+};
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
   };
-}
+};
